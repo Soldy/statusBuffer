6d10745fa95f97a4049ac93788b7958fa739831f
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-16 19:50:34 +0000

    statusrc support
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+/*
+ *  @Soldy\statusBuffer\2021.01.16\GPL3
+ */
 'use strict';
 const confrc = new (require('confrc')).confrc();
 const poolrc = (require('poolrc')).poolrc;
 const http = require('http');
+const src = new (require('statusrc')).statusrc;
 
 const statusPool = new poolrc(10000);
 
+/*
+ * @prototype
+ */
 const serverBase = function(){
+    /*
+     *  @private
+     *  @return {integer}
+     */
     const start=function(){
         http.createServer(function (req, res) {
             let post = '';
@@ -14,83 +25,56 @@ const serverBase = function(){
             });
             req.on('end', async function () {
                 if(req.url === '/')
-                    return res.end();
+                    return src.notExist(res);
                 if(req.method === 'GET')
                     return getCall(post, req, res);
                 if(req.method === 'POST')
                     return postCall(post, req, res);
-                return notAllowedMethod(res);
+                return src.notAllowedMethod(res);
             });
         }).listen(
             confrc.get('statusBuffer').httpd.port,
             confrc.get('statusBuffer').httpd.address
         );
     };
+    /*
+     *  @param {object} post
+     *  @param {object} req
+     *  @param {object} res
+     *  @private
+     *  @return {integer}
+     *
+     */
     const getCall = function(post, req, res){
         if(!statusPool.check(req.url))
-            return notExist(res);
-        res.writeHead(200);
-        res.write(
-            JSON.stringify(
-                statusPool.get(req.url)
-            )
+            return src.notExist(res);
+        return src.end(
+           res,
+           200,
+           statusPool.get(req.url)
         );
-        return res.end();
-    }
+    };
+    /*
+     *  @param {object} post
+     *  @param {object} req
+     *  @param {object} res
+     *  @private
+     *  @return {integer}
+     *
+     */
     const postCall = function(post, req, res){
         try{
             post = JSON.parse(post);
         }catch(e){
-            return badRequest(res);
+            return src.badRequest(res);
         }
         statusPool.set(
             req.url,
             post
         );
-        return end(res);
-    }
-    const end = function(res){
-        res.writeHead(200);
-        res.write(
-            JSON.stringify({
-                'result':'ok'
-            })
-        );
-        return res.end();
-
+        return src.ok(res);
     };
-    const notExist = function(res){
-        res.writeHead(405);
-        res.write(
-            JSON.stringify({
-                'result':'Method Not Allowed'
-            })
-        );
-        return res.end();
-    };
-    const notAllowedMethod = function(res){
-        res.writeHead(405);
-        res.write(
-            JSON.stringify({
-                'result':'Method Not Allowed'
-            })
-        );
-        return res.end();
-    };
-    const badRequest = function(res){
-        res.writeHead(400);
-        res.write(
-            JSON.stringify({
-                'result':'bad request'
-            })
-        );
-        return res.end();
-    };
-    const stop=function(){ 
-        http.close();
-    };
-    process.on('EXIT', stop);
-    process.on('SIGINT', stop);
+    // constructor
     start();
 };
 
